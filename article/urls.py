b9b384bb54d25f99ff0dfb8286ded3d081515615
Anton-Joe <anton_yejx@hotmail.com>
2020-03-18 21:33:33 +0800

    创建自定义convert

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,28 @@
-from django.urls import re_path,path
+from django.urls import re_path, path, converters, register_converter
 from . import views
+
+
+class CategoryConverter(object):
+    regex = r'\w+|(\w+\+\w+)+/'
+
+    def to_python(self, value):
+        result = value.split('+')
+        return result
+
+    def to_url(self, value):
+        if isinstance(value, list):
+            result = '+'.join(value)
+            return result
+        else:
+            return RuntimeError('转换ULR，分类参数必须为列表')
+
+
+register_converter(CategoryConverter, 'cate')
+
+
 urlpatterns = [
-    re_path(r'^[^+/].',views.article_list),
-    path('', views.index)
+    # \w 0-9 , a-z ,A-z , _
+    # re_path(r'list/(?P<categories>\w+|(\w+\+\w+)+)/' ,views.article_list),
+    path('', views.index),
+    path('list/<cate:categories>', views.article_list, name='list')
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import reverse
 
 
 def index(request):
@@ -6,5 +7,11 @@ def index(request):
 
 
 def article_list(request, categories):
+    # 函数传进来的categories，经过了处理：categories = CategoryConverter.to_python(categories)
+
+    # reverse下的categories，经过了处理：categories = CategoryConverter.to_url(categories)
+    path = reverse('list', kwargs={'categories': categories})
+    print(path)
+
     text = "您填写的分类是 %s" % categories
     return HttpResponse(text)
